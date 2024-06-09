cf6d73a91b1311532a078d19a138222ce21d9577
Ahmed Hosny <ahmhosny2002@gmail.com>
2024-06-10 00:12:39 +0300

    Testing...

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from fastapi.responses import FileResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.docs import get_swagger_ui_html
 from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
 
 from authentication.http import get_auth_header, authenticate_kerberos
 from authentication.kerberos import KerberosMiddleware, create_access_token, get_current_user
@@ -32,6 +33,7 @@ app.add_middleware(
 )
 
 app.add_middleware(KerberosMiddleware)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 db_instance.reload()
 
@@ -66,15 +68,6 @@ def favicon():
     return FileResponse("favicon.ico")
 
 
-@app.get("/docs1", include_in_schema=False)
-async def custom_swagger_ui_html():
-    return get_swagger_ui_html(
-        openapi_url="/openapi.json",
-        title="Custom Swagger UI",
-        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui-bundle.js",
-        swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui.css",
-    )
-
 @app.get("/openapi.json", include_in_schema=False)
 async def custom_openapi():
     return get_openapi(
@@ -90,3 +83,8 @@ async def generate_token(request: Request):
     principal = request.state.principal
     access_token = create_access_token(data={"sub": principal})
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/docs1", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return HTMLResponse(content=open("static/custom_swagger.html").read())
